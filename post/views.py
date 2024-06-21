131df108a872bed0da8a0c16ec517db53e19fd64
Dagim-Zeynu <dagim35z@gmail.com>
2024-06-21 11:43:35 +0300

    first commit

---
+++ post/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
